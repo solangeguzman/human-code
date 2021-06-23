2ca9ae4b6ed6a9d13eb7b5147b2922d9841ac175
solangeguzman <solangeguzmanduchen@gmail.com>
2021-06-23 17:07:47 +0200

    human-code

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// 3. Gli amici ti hano appena invitato a passare
+//  con loro il sabato al mare, unirsi alla comitiva.
+1 -controllo il telefono, 
+2- vedo che c'è un messaggio nuovo in arrivo
+3- vado a leggerlo e controllo chi sono gli altri invitati.
+4- in base a quello, decido se andare o meno.
+5- mediante un messaggio decido di avvisare se andare o meno.
+6- con quale mezzo di trasporto ci si sposta , se in macchina , treno . 
+7 -if(vado)
+     Aggiungo il mio nome alla lista
+else(no non ci vado)
+            no non aggiungete il mio nome
+8-function = scrivere(testo del messaggio)  (ricevitore)(invio del messaggio)
